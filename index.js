3137fcd960247f41aebaecb75b0bdb791b3f18c7
jaquemoura <jaquelinemm1@hotmail.com>
2019-04-19 20:56:24 -0300

    Nova rota adicionada

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.status(200).send({ message: 'Olá mundo esse é meu novo repositorio clonado.'})
+});
+
+app.get('/ola', (req, res) => {
+    res.status(200).send({ message: 'Olá'});
+})
+
+app.listen(4001, () => {
+    console.log()'Api inicializado na porta 4001');
+}
